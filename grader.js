2fef60603e30465066823acf094be0aab2838753
Eugene Skinner <eskinfrn@gmail.com>
2015-06-04 15:46:08 -0600

    refactored, fixed the loose equality issue. I also linted the code with jshint and the tests passed with jasmine

--- grader.js
+++ grader.js
@@ -18,7 +18,7 @@ module.exports = {
     var sum = 0;
     arr.forEach(function(el){
       sum = sum + el;
-    })
+    });
     return (sum/arr.length);
   },
 
@@ -35,22 +35,22 @@ module.exports = {
   },
 
   modeScore: function mode(arr){
-    var modeObj = {};   //Create an Object
-    var max = arr[0]; //Create a variable for the value of the number that occurs the most often set it to array[0]
-    var maxCount = 1;   //created a counter, set to one to account the first instance of the number at array[0]
-    arr.forEach(function(el){  //forEach to traverse the values of the array
-        if(modeObj[el] == null){ //if the object is empty or does not have the current element as a key, create a key and set the value to 1 for the first count
+    var modeObj = {};
+    var max = arr[0];
+    var maxCount = 1;
+    arr.forEach(function(el){
+        if(modeObj[el] === undefined){
             modeObj[el] = 1;
         }
-        else{                    //Otherwise add one to the value of the key modeObj[el]
+        else{
             modeObj[el]++;
         }
-        if(modeObj[el] > maxCount){  // if the value at modeObj[el] is greater than the maxCount set the max = el and maxCount = to the modeObj[el]
+        if(modeObj[el] > maxCount){
             max = el;
             maxCount = modeObj[el];
         }
-    })
-     return max;                    //At the end of it all return the max
+    });
+     return max;
    }
 
-}
+};
