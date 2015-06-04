0fad939b6ee84c021f7d8a72de1a6bd7767077be
Eugene Skinner <eskinfrn@gmail.com>
2015-06-04 11:35:42 -0600

    All Functions and test complete.. Haven't been able to get the multi-modal stretch goal

--- spec/spec.js
+++ spec/spec.js
@@ -1,7 +1,7 @@
 var gradeBook = require('./../grader.js');
 
   describe('LetterGrader Function', function(){
-    xit('Letter Grade should reduce every 10 points', function(){
+    it('Letter Grade should reduce every 10 points', function(){
       expect(gradeBook.letterGrader(90)).toEqual('A');
       expect(gradeBook.letterGrader(80)).toEqual('B');
       expect(gradeBook.letterGrader(70)).toEqual('C');
@@ -11,7 +11,7 @@ var gradeBook = require('./../grader.js');
   })
 
   describe('averageGrader Function', function(){
-    xit('Returns the average score from an array of scores', function(){
+    it('Returns the average score from an array of scores', function(){
       expect(gradeBook.averageScore([90, 95, 87, 60])).toEqual(83);
     })
   })
@@ -21,5 +21,14 @@ var gradeBook = require('./../grader.js');
       expect(gradeBook.medianScore([52,80,80,86,94])).toEqual(80);
       expect(gradeBook.medianScore([52,80,86,94])).toEqual(83);
       expect(gradeBook.medianScore([80,52,86,94,80])).toEqual(80);
+    })
+  })
+
+  describe('modeScore Function', function(){
+    it('Returns the score that occurs most often from an array', function(){
+      expect(gradeBook.modeScore([82, 92, 75, 91, 92, 89, 95, 100, 86])).toEqual(92);
+      expect(gradeBook.modeScore([100,100,100,100,80,80,80,80,80])).toEqual(80);
+      //Stretch Goal - Multi Modal.
+      //expect(gradeBook.modeScore([50,50,50,50,50,60,60,60,60,60])).toEqual(60);
     })
   })
